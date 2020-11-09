ef4ff5123f457f41a09e535abebe0c5486303047
D3v <deviljin112@users.noreply.github.com>
2020-11-09 11:12:14 +0000

    Task Completed

--- modules.py
+++ modules.py
@@ -1,3 +1,22 @@
+# Task
+# get user input of a float number
+# check if the number is lower than .50 then round the figure to lower end
+# check if the number is greater than .51 then round the figure to higher end
+# example - num_float = 23.66 - round it to 24, num_float = 23.50 - round it to lower end
+
+import math
 import random
 
-print(random.random())
+try:
+    number = float(input("Input a Float Number.\n=> "))
+except:
+    print("That is not a float value!")
+else:
+    print(f"Starting number: {number}")
+
+    if number % 1 < .5:
+        output = math.floor(number)
+    else:
+        output = math.ceil(number)
+
+    print(f"Formatted number: {output}")
